b3a2174df530c43736ade2a4dddf7fd24d9715c3
DmitriyAxi <48443463+DmitriyAxi@users.noreply.github.com>
2020-10-25 23:37:26 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    var numStr = n.toString().split('');
+    var j = numStr.length;
+    var tmp;
+    var t =0;
+    for(let i = 0; i < --j; ){
+
+        tmp = numStr[i];
+        console.log(i + "" + j);
+        numStr[i] = numStr[j];
+        numStr[j] = tmp;
+        i++;
+
+    }
+    return parseInt(numStr.join(""));
 }
